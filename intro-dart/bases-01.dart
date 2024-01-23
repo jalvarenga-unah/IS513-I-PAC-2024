bbac4920c6199701417b647a6089a5f519595294
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-01-23 17:46:37 -0600

    principios de Dart

---
+++ intro-dart/bases-01.dart
@@ -0,0 +1,26 @@
+void main() {
+  //variables
+  final nombre; // declarar con un valor nulo
+  nombre = 'Juan perez'; // inicializar una vez
+//   nombre = 'Juan Alvarenga'; // ya no es permitido
+  const edad = 29;
+
+  final double estatura = 1.60;
+
+  print(nombre);
+  print(edad);
+
+  final numeros = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    "hola",
+    true,
+    1.23,
+    [1, 2]
+  ];
+
+  final mascota = {};
+}
