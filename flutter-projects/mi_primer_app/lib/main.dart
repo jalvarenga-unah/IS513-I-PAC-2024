fd2e24a149f11e2e6119c1b3a079e14ff8aa0c49
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-02-08 17:21:27 -0600

    Trabajando con disenios, Column, Container, SizedBox

--- flutter-projects/mi_primer_app/lib/main.dart
+++ flutter-projects/mi_primer_app/lib/main.dart
@@ -4,31 +4,48 @@ import 'package:flutter/material.dart';
 // StatefulWidget: Puede cambiar su estado
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mi primer App',
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: const Text(
             'Mi primer App',
-            style: TextStyle(color: Colors.white),
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
           //rgb(186, 104, 200)
           // backgroundColor: Color.fromRGBO(186, 104, 200, 1),
-          backgroundColor: Color(0xFFAB47BC),
+          backgroundColor: const Color(0xFFAB47BC),
         ),
-        body: const Center(
-          child: Text('Hola Mundo'),
+        body: const SizedBox(
+          width: double.infinity,
+          child: Column(
+            // mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Hizo tantas veces clic:'),
+              Text('10'),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(
+          // backgroundColor: Colors.deepPurple[100],
+          onPressed: () {
+            print('Hizo clic ');
+          },
+          child: const Icon(
             Icons.add,
             color: Colors.deepPurple,
           ),
