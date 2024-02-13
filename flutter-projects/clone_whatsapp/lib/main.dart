55f8e9d492ad3145581e81ec150cb4cd9aac2a38
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-02-13 16:54:27 -0600

    inicio de nuevo proyecto, clone_whatsapp

---
+++ flutter-projects/clone_whatsapp/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:clone_whatsapp/src/modules/principal/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
