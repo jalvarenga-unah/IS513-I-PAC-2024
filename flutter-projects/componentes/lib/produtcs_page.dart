bab1964da6dad9cb60e057b8f2c1043c30696381
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-03-04 18:51:14 -0600

    Peticiones http

--- flutter-projects/componentes/lib/produtcs_page.dart
+++ flutter-projects/componentes/lib/produtcs_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/models/producto.dart';
 import 'package:componentes/providers/productos_provider.dart';
 import 'package:flutter/material.dart';
 
@@ -8,23 +9,83 @@ class ProductsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final provider = ProductosProvider();
 
-    final productos = provider.getProdutcs();
+    // final productos = provider.getProdutcs();
 
     // FutureBuilder
 
-    print(productos);
+    // print(productos);
 
     return Scaffold(
         appBar: AppBar(
           title: const Text('Productos'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              Text('Productos'),
-            ],
-          ),
+        body: FutureBuilder(
+          future: provider.getProdutcs(),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<Producto>> snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('ocurrio algo inesperado: ${snapshot.error}'),
+              );
+            }
+            if (snapshot.hasData) {
+              final productos = snapshot.data!;
+
+              return ItemProduct(productos: productos);
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
         ));
+  }
+}
+
+class ItemProduct extends StatelessWidget {
+  const ItemProduct({
+    super.key,
+    required this.productos,
+  });
+
+  final List<Producto> productos;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: productos.length,
+      itemBuilder: (context, index) {
+        final producto = productos[index];
+
+        return Card(
+          margin: const EdgeInsets.all(10),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Center(
+                child: Image.network(
+                  producto.image,
+                  height: 150,
+                  width: 150,
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                producto.title,
+                style: const TextStyle(
+                    fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                producto.description.length > 100
+                    ? producto.description.substring(0, 50)
+                    : producto.description,
+              ),
+            ]),
+          ),
+        );
+      },
+    );
   }
 }
