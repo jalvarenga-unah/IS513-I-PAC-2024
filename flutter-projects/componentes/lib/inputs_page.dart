89b7c6d9312d19afac2bf329f5cd12b6398bf4eb
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-03-11 17:52:03 -0600

    Trabajando con Formularios

--- flutter-projects/componentes/lib/inputs_page.dart
+++ flutter-projects/componentes/lib/inputs_page.dart
@@ -6,45 +6,87 @@ class InputsPage extends StatelessWidget {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  final nombreController = TextEditingController();
+  final apellidoController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Inputs'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const FlutterLogo(size: 160),
-              const SizedBox(height: 20),
-              TextField(
-                controller: emailController,
-                maxLength: 30,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  label: Text('Email'),
-                  icon: Icon(Icons.email),
-                  border: OutlineInputBorder(),
+        appBar: AppBar(
+          title: const Text('Inputs'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const FlutterLogo(size: 160),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: emailController,
+                  maxLength: 30,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    label: Text('Email'),
+                    icon: Icon(Icons.email),
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              CustomInput(
-                controller: passwordController,
-                obscureText: true,
-              ),
-              CustomInput(controller: emailController),
-            ],
+                const SizedBox(height: 20),
+                CustomInput(
+                  controller: passwordController,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 20),
+                Text('formulario'),
+                const Divider(),
+                Form(
+                  key: formKey, // identificador del formulario "controller"
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: nombreController,
+                        maxLength: 20,
+                        validator: (value) {
+                          if (value!.isEmpty) return 'El nombre es obligatorio';
+
+                          if (value.length < 3)
+                            return 'El nombre debe tener al menos 3 caracteres';
+
+                          return null;
+                        },
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                          label: Text('Nombre'),
+                          icon: Icon(Icons.person),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            //acceder al valor del email
+            print(emailController.text);
+
+            if (formKey.currentState!.validate()) {
+              print('formulario valido');
+              //TODO: LOGICA PARA GUARDAR LOS DATOS
+            }
+          },
+          child: const Icon(Icons.clear),
+        ));
   }
 }
 
 class CustomInput extends StatefulWidget {
-  const CustomInput({
+  CustomInput({
     super.key,
     required this.controller,
     this.obscureText = false,
@@ -70,7 +112,6 @@ class _CustomInputState extends State<CustomInput> {
         suffixIcon: IconButton(
             onPressed: () {
               // TODO: logica para mostrar u ocultar el pasword
-              obscureText = !widget.obscureText;
             },
             icon: const Icon(Icons.remove_red_eye_rounded)),
         border: const OutlineInputBorder(),
